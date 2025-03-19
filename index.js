6f32197c8fcea8f3131bbe2a657cf59a3de65e28
OsiaruRobert <osiarurobert@gmail.com>
2025-03-20 00:15:42 +0100

    node

--- index.js
+++ index.js
@@ -4,29 +4,32 @@ import express from 'express';
 
 // configuration
 const
-  cfg = {
+cfg = {
     port: process.env.PORT || 3100
-  };
-
+};
+let count = 1;
 // Express initiation
 const app = express();
 
 // home page route
 app.get('/', (req, res) => {
-     console.log(req.url);
+    console.log(req.url);
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.send(`<h1>Hello World!</h1>`);
-  
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+
+
+    count++;
+    console.log(count);
+
+    res.send(`${count}<h1>Hello World!</h1>`);
+
 });
 
 // serve static assets
-app.use(express.static( 'static' ));
+app.use(express.static('static'));
 
 // start server
 app.listen(cfg.port, () => {
-  console.log(`Example app listening at http://localhost:${ cfg.port }.`);
+    console.log(`Example app listening at http://localhost:${ cfg.port }.`);
 });
-
-
